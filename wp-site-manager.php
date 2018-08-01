88ddf3c5a6d1532f0c49fb3f15e55105507ade0f
Lms <lms-pgm@hotmail.fr>
2018-08-01 12:38:20 +0200

    bump version

--- wp-site-manager.php
+++ wp-site-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Site Manager
  * Plugin URI: http://bonnierpublications.com
  * Description: Service for the site manager
- * Version: 0.1.7
+ * Version: 0.1.10
  * Author: Michael Sørensen & Frederik Rabøl
  * Author URI: http://bonnierpublications.com
  */
