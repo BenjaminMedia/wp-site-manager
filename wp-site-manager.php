28329c3137173eb0958a4cbbcbf561ff71ee1f3d
Mads Paaskesen <madspaaskesen@gmail.com>
2022-09-09 16:37:17 +0200

    upgrade-acf-to-5.12.2
    update to php7.4 + composer packages etc.

--- wp-site-manager.php
+++ wp-site-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Site Manager
  * Plugin URI: http://bonnierpublications.com
  * Description: Service for the site manager
- * Version: 2.0.2
+ * Version: 2.0.3
  * Author: Michael Sørensen & Frederik Rabøl
  * Author URI: http://bonnierpublications.com
  */
